097c29970db124c15bca5ae4f8da040ab8a5090d
sadman <samsikder47@gmail.com>
2021-04-11 22:53:45 +0600

    cors error; state 1

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,52 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Login with github</h1>
+
+    <button @click="loginWithGithub">Login with github </button>
   </div>
 </template>
+
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios';
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  },
+  methods:{
+    async loginWithGithub(){
+      let url = process.env.VUE_APP_GITHUB_API+"?client_id="+process.env.VUE_APP_GITHUB_CLIENT_ID+"&redirect_uri="+process.env.VUE_APP_GITHUB_API_CALLBACK
+      // console.log(process.env.VUE_APP_GITHUB_API)
+      // axios.get(url).then( response =>{
+      //   console.log(response);
+      // })
+
+      const config = {
+        crossdomain: true,
+        baseURL: 'http://localhost:8080',
+        timeout: 10000,
+        withCredentials: true,
+        // transformRequest: [(data) => JSON.stringify(data.data)],
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Headers': '*',
+        }
+
+      }
+
+      await axios.get(url, config).then( response =>{
+        console.log(response);
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
